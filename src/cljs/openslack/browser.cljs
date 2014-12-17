d16f4c6c54ddb93d37458e662c8f9bc5ae1a7cac
Andrey Antukh <niwi@niwi.be>
2014-12-17 19:29:51 +0100

    Fix scrolling.

--- src/cljs/openslack/browser.cljs
+++ src/cljs/openslack/browser.cljs
@@ -4,11 +4,22 @@
   []
   (= (.-permission js/Notification) "granted"))
 
+(defn- calculate-max-scroll
+  [element]
+  (let [scroll-height (.-scrollHeight element)
+        outer-height (.-offsetHeight element)]
+    (- scroll-height outer-height)))
+
 (defn scroll-to-bottom
   [query]
   (let [element (.querySelector js/document query)
-        height (.-scrollHeight element)]
-    (set! (.-scrollTop element) height)))
+        maxscroll (calculate-max-scroll element)]
+    (set! (.-scrollTop element) maxscroll)
+    (js/setTimeout (fn []
+                     (let [maxscroll (calculate-max-scroll element)]
+                       (set! (.-scrollTop element) maxscroll)))
+                   1000)))
+
 
 (defn notify
   [title body icon]
