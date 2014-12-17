57dd63a612f3ec2e4383ecfd5091a9d9c000db2c
Alejandro Gómez <alejandro@dialelo.com>
2014-12-17 12:01:34 +0000

    Show availability in human-readable form when there isn't a status string

--- src/cljs/openslack/views/user.cljs
+++ src/cljs/openslack/views/user.cljs
@@ -3,6 +3,12 @@
             [sablono.core :as s :include-macros true]
             [openslack.state :as st]))
 
+(def availability->default-status
+  {:available "Online"
+   :unavailable "Offline"
+   :dnd "Busy"
+   :xa "Busy"})
+
 (defn user [state owner]
   (reify
     om/IDisplayName
@@ -15,7 +21,7 @@
               presence (st/get-presence @st/state {:jid jid})]
           (s/html
            [:div.active-user
-                                        ; TODO: avatar
+            ;; TODO: avatar
             [:img
              {:height "50",
               :width "50",
@@ -23,8 +29,11 @@
               :src "/static/imgs/placerholder-avatar-1.jpg"}]
             [:div.square
              [:div.row [:h2 (:local jid)]]
-             (let [status-text [:p.status-text (:status presence)]]
-               (condp = (:availability presence :available)
+             (let [availability (:availability presence :available)
+                   default-status (availability->default-status availability)
+                   status (:status presence)
+                   status-text [:p.status-text (if status status default-status)]]
+               (condp = availability
                  :available [:div.row [:div.status.online] status-text]
                  :unavailable [:div.row [:div.status.offline] status-text]
                  :dnd [:div.row [:div.status.busy] status-text]))]]))))))
