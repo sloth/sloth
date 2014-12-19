7d7589f1fd219362cfcadf2bc4199e35d3309c37
Andrey Antukh <niwi@niwi.be>
2014-12-19 20:34:12 +0100

    Set /debug and /release endpoints on gulp:serve task instead of having everything on / (root).

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ var paths = {};
 paths.dist = "resources/public/";
 paths.app = "resources/assets/";
 paths.js = [
-    paths.app + "js/stanza/stanzaio.bundle.min.js"
+    paths.app + "js/stanza/stanzaio.bundle.min.js",
+    paths.app + "js/react/react-0.11.2.min.js"
 ];
 paths.scss = paths.app + "scss/**/*.scss";
 
@@ -59,12 +60,21 @@ gulp.task("serve", function(){
     var express = require("express");
 
     var app = express(),
-        resources = __dirname + "/" + paths.dist;
+        resources = __dirname + "/resources/";
 
-    app.use("/sandbox", express.static(resources+"sandbox/"));
-    app.use("/static", express.static(resources));
-    app.all("/*", function(req, res, next){
-        res.sendFile("index.html", {root: resources});
+    app.use("/sandbox", express.static(resources + "sandbox/"));
+    app.use("/static", express.static(resources + "public/"));
+
+    // app.all("/*", function(req, res, next){
+    //     res.sendFile("index.html", {root: resources});
+    // });
+
+    app.all("/debug/*", function(req, res, next){
+        res.sendFile("index.debug.html", {root: resources});
+    });
+
+    app.all("/release/*", function(req, res, next){
+        res.sendFile("index.release.html", {root: resources});
     });
 
     app.listen(9000);
