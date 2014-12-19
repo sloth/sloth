36ff856783eeeb94846e2b0c00ea54df9b810835
Andrey Antukh <niwi@niwi.be>
2014-12-19 20:33:32 +0100

    Add initial work on stanza.io extern file for make possible use :advanced compilation mode.

---
+++ resources/assets/externs/stanza.io.js
@@ -0,0 +1,12 @@
+
+var XMPP = {};
+
+XMPP.prototype.createClient = function(config) {};
+XMPP.prototype.connect = function() {};
+XMPP.prototype.disconnect = function() {};
+XMPP.prototype.sendPresence = function(a, b) {};
+XMPP.prototype.sendMessage = function(a) {};
+XMPP.prototype.sendIq = function(a) {};
+XMPP.prototype.on = function(a, b) {};
+XMPP.prototype.off = function(a) {};
+
