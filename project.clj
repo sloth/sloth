c7ee8e4e452fbbe22886e11fbc7d752eb1a75dea
Andrey Antukh <niwi@niwi.be>
2015-01-15 19:21:02 +0100

    WIP: improve project.clj

--- project.clj
+++ project.clj
@@ -1,44 +1,60 @@
-(defproject sloth "0.1.0-SNAPSHOT"
+(defproject sloth "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "BSD (2-Clause)"
             :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+
+  :dependencies [[org.clojure/clojure "1.6.0" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2665" :scope "provided"]
                  [org.clojure/tools.namespace "0.2.7"]
 
                  ;; Backend dependencies
                  [compojure "1.3.1"]
+                 [cc.qbits/jet "0.5.1"]
                  [ring/ring-core "1.3.2" :exclusions [javax.servlet/servlet-api
                                                       org.clojure/tools.reader]]
-                 [cc.qbits/jet "0.5.1"]
-
 
                  ;; Frontend dependencies
-                 [org.clojure/clojurescript "0.0-2665"]
-                 [org.om/om "0.8.0"]
-                 [secretary "1.2.1"]
-                 [sablono "0.2.22" :exclusions [com.facebook/react]]
-                 [hodgepodge "0.1.1" :exclusions [org.clojure/clojurescript]]
-                 [cuerdas "0.1.0"]
-                 [shodan "0.4.1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
 
+                 ;; [com.facebook/react "0.12.2.1"]
+                 [org.om/om "0.8.0" :exclusions [org.clojure/clojurescript]]
+                 [secretary "1.2.1" :exclusions [org.clojure/clojurescript]]
+                 [sablono "0.2.22" :exclusions [com.facebook/react org.clojure/clojurescript]]
+                 [hodgepodge "0.1.2"]
+                 [cats "0.2.0" :exclusions [org.clojure/clojure
+                                            org.clojure/clojurescript]]
+                 [cuerdas "0.1.0" :exclusions [org.clojure/clojurescript]]
+                 [shodan "0.4.1" :exclusions [org.clojure/clojurescript]]
+                 [rum "0.2.0"]
+
+                 ;; Commented due to temporary compatibility issue with the
+                 ;; last clojurescript compiler version.
                  ;; [com.cemerick/piggieback "0.1.3"]
                  ;; [weasel "0.4.2"]
 
-                 ;; Shared dependencies
-                 [hiccup-bridge "1.0.1"]
-                 [cats "0.2.0" :exclusions [org.clojure/clojure]]]
+                 ;; ;; Shared dependencies
+                 [hiccup-bridge "1.0.1" :exclusions [org.clojure/clojurescript]]]
 
   :source-paths ["src/clj"]
+  :jar-exclusions [#"user.clj"]
+  :target-path "target/%s"
+
+  ;; Commented due to temporary compatibility issue with the
+  ;; last clojurescript compiler version.
   ;; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
-  :plugins [[lein-cljsbuild "1.0.4"]
-            [hiccup-bridge "1.0.1"]]
+
+  :clean-targets ^{:protect false} ["target" "resources/public/js/app.js"
+                                    "resources/public/js/out/"]
+
+  :plugins [[lein-cljsbuild "1.0.4" :exclusions [org.clojure/clojure]]
+            [hiccup-bridge "1.0.1" :exclusions [org.clojure/clojurescript]]]
+
   :cljsbuild {:builds
               [{:id "devel"
                 :source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/app.js"
-                           :output-dir "resources/public/js"
+                           :output-dir "resources/public/js/out"
                            :optimizations :none
                            :source-map true}}
 
@@ -46,16 +62,8 @@
                 :source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/app.js"
                            :optimizations :whitespace
-                           :pretty-print true}}
+                           :static-fns false
+                           :pretty-print true}}]}
 
-               {:id "release"
-                :source-paths ["src/cljs"]
-                :compiler {:output-to "resources/public/js/app.js"
-                           :optimizations :advanced
-                           :pretty-print false
-                           :externs ["react/externs/react.js"
-                                     "assets/externs/stanza.io.js"]}}]}
-
-  :jar-exclusions [#"user.clj"]
-  :target-path "target/%s"
   :profiles {:standalone {:main ^:skip-aot sloth.core}})
+
